82d341d64cf2429ec17ea25be19f431e5b6621d7
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-08-09 18:58:27 +0200

    Update FeedbackSection.jsx

--- src/components/Feedback/FeedbackSection/FeedbackSection.jsx
+++ src/components/Feedback/FeedbackSection/FeedbackSection.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
-import React, { Component } from 'react';
 import { Statistics } from '../Statistics/Statistics';
 import { Notification } from '../Notification/Notification';
 import {
@@ -9,63 +8,66 @@ import {
   Title,
   TitleStatistic,
 } from './FeedbackSection.styled';
+import { useState } from 'react';
 
-export class FeedbackSection extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const FeedbackSection = ({ title, titleStatistic }) => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleFeedback = e => {
+  const handleFeedback = e => {
     const eventName = e.target.textContent.toLowerCase();
-    this.setState(prevState => {
-      return {
-        [eventName]: prevState[eventName] + 1,
-      };
-    });
+    switch (eventName) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const { handleFeedback } = this;
+  const CountTotalFeedback = () => good + neutral + bad;
 
-    const CountTotalFeedback = () => {
-      return good + neutral + bad;
-    };
+  const PositiveFeedback = () => {
+    let interestSumm = Math.round((good / CountTotalFeedback()) * 100);
+    const interest = interestSumm > 0 ? interestSumm : 0;
+    return interest;
+  };
 
-    const PositiveFeedback = () => {
-      let interestSumm = Math.round((good / CountTotalFeedback()) * 100);
-      const interest = interestSumm > 0 ? interestSumm : 0;
-      return interest;
-    };
-
-    return (
-      <Section>
-        <Container>
-          <Title>{this.props.title}</Title>
-          <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={handleFeedback}
+  return (
+    <Section>
+      <Container>
+        <Title>{title}</Title>
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+        <TitleStatistic>{titleStatistic}</TitleStatistic>
+        {CountTotalFeedback() === 0 && (
+          <Notification message={'There is no feedback'}></Notification>
+        )}
+        {CountTotalFeedback() !== 0 && (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            summFeedback={CountTotalFeedback()}
+            positiveFeedbac={PositiveFeedback()}
           />
-          <TitleStatistic>{this.props.titleStatistic}</TitleStatistic>
-          {CountTotalFeedback() === 0 && (
-            <Notification message={'There is no feedback'}></Notification>
-          )}
-          {CountTotalFeedback() !== 0 && (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              summFeedback={CountTotalFeedback()}
-              positiveFeedbac={PositiveFeedback()}
-            />
-          )}
-        </Container>
-      </Section>
-    );
-  }
-}
+        )}
+      </Container>
+    </Section>
+  );
+};
 
 FeedbackSection.propTypes = {
   title: PropTypes.string.isRequired,
